a4ba7487c5db460ca75094f64e302dec1bf20ccc
ayan khan <ayankhan8223@gmail.com>
2021-07-05 19:48:23 +0530

    ready

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require("helmet");
 const MongoStore = require('connect-mongo');
 
-const dburl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+const dburl = process.env.DB_URL
 
 // const urlDB = process.env.DB_URL;
 // 'mongodb://localhost:27017/yelp-camp'
